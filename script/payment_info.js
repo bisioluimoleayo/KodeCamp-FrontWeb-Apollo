47de56b9af2d8b206f6e6a2c6f26de61705e21cb
bisioluimoleayo <bisioluimoleayo1@gmail.com>
2021-10-19 19:09:56 +0100

    first commit

---
+++ script/payment_info.js
@@ -0,0 +1,13 @@
+
+const targetDiv = document.getElementById("card_detials");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
+
+
+
---
+++ script/redirect.js
@@ -0,0 +1,3 @@
+function Redirect() {
+    window.location.assign("index_login.html"); 
+ }
